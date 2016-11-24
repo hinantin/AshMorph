49de624f90e05b9b908483bae4ebb5671bf32318
rcastromamani <richard.castro.mamani@gmail.com>
2016-11-23 19:20:46 -0500

    Adidng more vocab, and changes for verbalized nouns/adjectives.

--- extractEntries.pl
+++ extractEntries.pl
@@ -5,6 +5,7 @@ use warnings;
 use utf8;
 binmode STDIN, ':utf8';
 binmode STDOUT, ':utf8';
+use Getopt::Long qw(GetOptions);
 
 my $filename = 'n-vroot.prq.foma';
 open(my $fh, '>', $filename) or die "Could not open file '$filename' $!";
@@ -14,18 +15,39 @@ print $fh  "# Verbalized nouns\n";
 print $fh  "# -----------------\n";
 print $fh  "define NounToVerbPRQin [\n";
 my $count = 0;
-while (<>)
-{
+
+# Obtaining the options
+my @files;
+my $options = "--file file_1 --file file_2 ... ";
+GetOptions (
+'file=s' => \@files,
+) or die " Usage:  $0 $options\n";
+#if (@files) {
+#  print STDERR " Usage:  $0 $options\n";
+#  exit;
+#} 
+  
+my $file;
+while (defined($file = shift @files)) {
+open INFO, $file or die "Could not open $file: $!";
+ while (<INFO>)
+ {
  if (m/N@->V/) { 
   if ($count == 0) { 
    my $text = $_;
    $text =~ s/^\|/ /g;
    print $fh $text; 
   }
-  else { print $fh $_; }
+  else { 
+   my $myvar = $_;
+   substr($myvar, 0, 1, "|") if " " eq substr($myvar, 0, 1);
+   print $fh $myvar; 
+  }
   $count++;
  }
  else {  }
+ }
+close(INFO);
 }
 print $fh  "];\n"; 
 close $fh;
