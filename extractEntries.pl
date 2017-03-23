2c45351e1b312e2dfc4851b046d3a00fd275f276
rcastromamani <richard.castro.mamani@gmail.com>
2017-03-23 18:33:33 -0500

    Adding more vocab.

--- extractEntries.pl
+++ extractEntries.pl
@@ -7,13 +7,16 @@ binmode STDIN, ':utf8';
 binmode STDOUT, ':utf8';
 use Getopt::Long qw(GetOptions);
 
-my $filename = 'n-vroot.prq.foma';
-open(my $fh, '>', $filename) or die "Could not open file '$filename' $!";
+my $outputfilename = 'n-vroot.prq.foma';
+my $flag = 'N@->V';
+my $title = 'Verbalized nouns';
+my $header = 'NounToVerbPRQin';
+open(my $fh, '>', $outputfilename) or die "Could not open file '$outputfilename' $!";
 
 print $fh  "# -----------------\n";
-print $fh  "# Verbalized nouns\n";
+print $fh  "# $title \n";
 print $fh  "# -----------------\n";
-print $fh  "define NounToVerbPRQin [\n";
+print $fh  "define $header [ \n";
 my $count = 0;
 
 # Obtaining the options
@@ -32,7 +35,7 @@ while (defined($file = shift @files)) {
 open INFO, $file or die "Could not open $file: $!";
  while (<INFO>)
  {
- if (m/N@->V/) { 
+ if (m/$flag/) { 
   if ($count == 0) { 
    my $text = $_;
    $text =~ s/^\|/ /g;
