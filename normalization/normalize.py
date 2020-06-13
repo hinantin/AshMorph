621041a7f2be3e8496656c97f6cefe9c2e1e8e7b
root <richard.castro.mamani@gmail.com>
2020-06-13 14:34:10 +0000

    normalization

---
+++ normalization/normalize.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+from foma import FST
+
+fst = FST.load('orthography.bin')
+word = 'ejAti'
+word = word.lower()
+numberresults = 0
+Results = []
+Results.append(word) 
+for result in fst.apply_up(word):
+    if result not in Results or numberresults < 1:
+        print result
+        numberresults += 1
+
