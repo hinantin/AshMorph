c80323b3f22a613886682ea05850bdfced5fb56f
root <richard.castro.mamani@gmail.com>
2020-06-15 02:44:52 +0000

    segmentation

--- segmentation/xfst2wapiti_morphTrain.pl
+++ segmentation/xfst2wapiti_morphTrain.pl
@@ -7,6 +7,8 @@ binmode STDIN, ':utf8';
 binmode STDERR, ':utf8';
 binmode STDOUT, ':utf8';
 use Storable;
+use File::Spec::Functions qw(rel2abs);
+use File::Basename;
 use Config::IniFiles;
 
 my $path = dirname(rel2abs($0));
