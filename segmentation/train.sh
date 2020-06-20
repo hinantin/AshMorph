d6e95ba1db9373512b03d84545a605a786eca2f0
root <richard.castro.mamani@gmail.com>
2020-06-20 22:17:32 +0000

    segmentation

--- segmentation/train.sh
+++ segmentation/train.sh
@@ -8,7 +8,7 @@
 cat /home/hinantin/ashaninka/AshaninkaMorph/segmentation/models/sentences.pan-ashaninka.xfst | /usr/bin/perl /home/hinantin/ashaninka/AshaninkaMorph/segmentation/xfst2wapiti_morphTrain.pl -1 -train > /home/hinantin/ashaninka/AshaninkaMorph/segmentation/models/pan-ashaninka.train.txt.model2
 cd /home/hinantin/ashaninka/Wapiti && /home/hinantin/ashaninka/Wapiti/wapiti train -p models/pan-ashaninka.pattern.txt.model2 -1 5 /home/hinantin/ashaninka/AshaninkaMorph/segmentation/models/pan-ashaninka.train.txt.model2 /home/hinantin/ashaninka/AshaninkaMorph/segmentation/models/pan-ashaninka.model2
 # model 3
-###cat /home/hinantin/ashaninka/AshaninkaMorph/segmentation/sentences.pan-ashaninka.xfst | /usr/bin/perl /home/hinantin/ashaninka/AshaninkaMorph/segmentation/xfst2wapiti_morphTrain.pl -2 -train > /home/hinantin/ashaninka/Wapiti/dat/pan-ashaninka.train.txt.model3
+###cat /home/hinantin/ashaninka/AshaninkaMorph/segmentation/models/sentences.pan-ashaninka.xfst | /usr/bin/perl /home/hinantin/ashaninka/AshaninkaMorph/segmentation/xfst2wapiti_morphTrain.pl -2 -train > /home/hinantin/ashaninka/AshaninkaMorph/segmentation/models/pan-ashaninka.train.txt.model3
 ###cd /home/hinantin/ashaninka/Wapiti && ./wapiti train -p dat/pan-ashaninka.pattern.txt.model3 -1 5 /home/hinantin/ashaninka/Wapiti/dat/pan-ashaninka.train.txt.model3 /home/hinantin/ashaninka/AshaninkaMorph/segmentation/models/pan-ashaninka.model3
 # model 4
 ###cat /home/hinantin/ashaninka/AshaninkaMorph/segmentation/models/sentences.pan-ashaninka.xfst | /usr/bin/perl /home/hinantin/ashaninka/AshaninkaMorph/segmentation/xfst2wapiti_morphTrain.pl -3 -train > /home/hinantin/ashaninka/AshaninkaMorph/segmentation/models/pan-ashaninka.train.txt.model4
