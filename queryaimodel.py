7c436fbf0675ef2bfaa72fb614db3c3e0a47731a
root <rcastro@hinant.in>
2024-12-08 12:00:28 -0500

    Adding a new changes

--- queryaimodel.py
+++ queryaimodel.py
@@ -6,5 +6,5 @@ genai.configure(api_key=os.environ["API_KEY"])
 model = genai.GenerativeModel('gemini-1.5-pro')
 
 input1 = input()
-response = model.generate_content("from this huge list of meaning, can any of this be interpreted as \"doctor or to cure\""+input1)
+response = model.generate_content("from this huge list of meaning, can any of this be interpreted as or is related to \"harvest\""+input1)
 print(response.text) 
