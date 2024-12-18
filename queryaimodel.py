45f6c7c0e7485da18f57bf66b028733cedf15e3e
root <rcastro@hinant.in>
2024-12-18 16:24:18 -0500

    Adding changes

--- queryaimodel.py
+++ queryaimodel.py
@@ -1,10 +1,11 @@
 import google.generativeai as genai
 import os
+import sys
 
 genai.configure(api_key=os.environ["API_KEY"])
 
 model = genai.GenerativeModel('gemini-1.5-pro')
-
+word = sys.argv[1]
 input1 = input()
-response = model.generate_content("from this huge list of meaning, can any of this be interpreted as or is related to \"harvest\""+input1)
+response = model.generate_content("from this huge list of meanings, can any of this be interpreted as or is related to \"{}\"".format(word)+input1)
 print(response.text) 
